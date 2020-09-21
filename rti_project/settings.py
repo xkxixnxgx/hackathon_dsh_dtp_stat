09b24caddbd53be4409dbf530c0dde62127df7e4
Andrei Kashin <kashin.andrew@gmail.com>
2020-09-21 13:43:57 +0300

    start rest api

--- rti_project/settings.py
+++ rti_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
+    'rest_framework',
     # project apps
     'account.apps.AccountConfig',
 ]
@@ -125,3 +127,9 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = ''
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
